cdfca43381d777432626057979d28b5f469740ea
IgorCremonezi <igc2009_guilherme@hotmail.com>
2024-08-31 09:26:28 -0300

    aula3108

---
+++ Lista3exerciciosLaravel/lista3/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/exer1', [ListaController::class, 'mostrarExer1']);
+
+Route::post('respostaExer1', [ListaController::class, 'calcularExer1']);
